f02926d02ac67116097d346c4e2e89d61e34a862
Jayon Kim <jpkim921@gmail.com>
2018-02-22 03:04:52 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
   attr_accessor :name, :posts
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -9,11 +11,18 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+      @@post_count += 1
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
     post.author = self
+    @@post_count += 1
   end
+
+  def self.post_count
+    @@post_count
+  end
+
 end
