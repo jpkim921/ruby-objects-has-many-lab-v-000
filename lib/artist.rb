190f9723f67dd500c4f615bee010940eceb0bf75
Jayon Kim <jpkim921@gmail.com>
2018-02-22 02:42:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,14 @@ class Artist
   def add_song(song)
     songs << song
     song.artist = self
-    # @@song_count += 1
+    @@song_count += 1
   end
 
   def add_song_by_name(name_of_song)
     song = Song.new(name_of_song)
     songs << song
     song.artist = self
-    # @@song_count += 1
+    @@song_count += 1
   end
 
 
